5af6cc0b6a4345acc77fd32bd15a856097fe76f6
misha <mishabelous11@gmail.com>
2020-09-18 18:36:11 +0300

    timeout ADDED

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
 const soundTrack = document.querySelectorAll('audio');
+const buttons = document.querySelectorAll('.btn');
 
 
 document.addEventListener('keydown', (event) => {
     soundTrack.forEach(function(element) {
     	if (element.id == event.keyCode) {
-    		element.play();
+          element.classList.add('playing')
+                setTimeout(() => {
+                    element.classList.remove('playing')
+                }, 120)
+                element.currentTime = 0
+               element.play()
     	}
     });
 })
 
 
-const buttons = document.querySelectorAll('.btn');
-
-
-document.addEventListener('click touchstart', (event) => {
+document.addEventListener('click', (event) => {
     const closestTarget = event.target.closest('.btn')
     if (closestTarget) {
-        buttons.forEach(function(element, index) {
+        buttons.forEach(function goplay(element, index) {
             if (element.lastElementChild.innerText == event.path[1].lastElementChild.innerHTML) {
-                soundTrack[index].play();
+                buttons[index].classList.add('playing')
+                setTimeout(() => {
+                    buttons[index].classList.remove('playing')
+                }, 120)
+                soundTrack[index].currentTime = 0
+                soundTrack[index].play()
             }
         });
+        
     }
 })
-
 
